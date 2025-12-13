df0a5781f2094c4873172863ab7ffd434fd82f21
Haerul Yuda Aditiya <haerulyudaaditiya@gmail.com>
2025-12-13 16:37:25 +0700

    Initial setup: React 19 + Tailwind v4 + DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      {' '}
+      <div className="p-10 flex flex-col gap-5 items-center justify-center min-h-screen bg-base-200">
+        <h1 className="text-3xl font-bold text-blue-600 underline">
+          Cek Tailwind v4
+        </h1>
+
+        <button className="btn btn-primary shadow-lg">Tombol DaisyUI</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
